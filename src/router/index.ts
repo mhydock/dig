49f339a6916fc426dcac4b124d7f85daddf98e0a
M Matthew Hydock <mhydock@yahoo.com>
2025-05-25 22:37:15 -0400

    change base url of router

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
